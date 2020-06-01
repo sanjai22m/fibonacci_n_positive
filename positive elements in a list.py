14d63e4202c43e70e7551089f609eba71a7ef05b
Sanjai M <66155684+sanjai22m@users.noreply.github.com>
2020-06-01 17:01:19 +0530

    Update positive elements in a list.py

--- positive elements in a list.py
+++ positive elements in a list.py
@@ -3,8 +3,8 @@ n=int(input('enter the number of elements in the list:'))
 for i in range(0,n):
     x=int(input())
     list1.append(x)
-pos_list=[]
+list2=[]
 for i in list1:
     if i>0:
-        pos_list.append(i)
-print(pos_list)
+        list2.append(i)
+print(list2)
